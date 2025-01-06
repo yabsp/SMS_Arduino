f4d109575e178f4f1c6b61e79a137beab75d12ac
MaxReinert <max.reinert@unibas.ch>
2025-01-06 10:08:59 +0100

    adds flags for more keypresses

--- src/libraries/Keyboard/KeyboardFlags.h
+++ src/libraries/Keyboard/KeyboardFlags.h
@@ -3,9 +3,25 @@
 
 #include <Arduino.h>
 
-extern volatile bool enterKeyPressed;
-extern volatile bool deleteKeyPressed;
-extern volatile bool keyPressDetected; // New flag for single key detection
+// Flags for special keys
+extern volatile bool enterKeyPressed; // Send SMS when in chat window
+extern volatile bool deleteKeyPressed; // Delete last typed character
+extern volatile bool escKeyPressed; // Go back to last scene?
+extern volatile bool tabKeyPressed; // ?                                        Key might not work
+extern volatile bool numLockKeyPressed; // ?      
+
+extern volatile bool arrowUpPressed; // Scroll one page up in chat              Key missing
+extern volatile bool arrowDownPressed; // Scroll one page down in chat          Key missing
+extern volatile bool arrowRightPressed; // ?                                    Key missing
+extern volatile bool arrowLeftPressed; // ?                                     Key missing
+
+extern volatile bool volumeUpKeyPressed; // Increase speaker volume             Key not known
+extern volatile bool volumeDownKeyPressed; // decrease speaker volume           Key not known
+
+extern volatile bool brightnessUpKeyPressed; // Increase screen brightness      Key not known
+extern volatile bool brightnessDownKeyPressed; // decrease screen brightness    Key not known
+
+extern volatile bool keyPressDetected; // New flag for single key detection     
 extern char lastKeyPressed;            // Last key pressed
 extern char message[];                 // Shared message buffer
 
